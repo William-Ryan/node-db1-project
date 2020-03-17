d7c753d89f70a3a4966725a978ea8a858da43cf4
William <billy.ryan.rom@gmail.com>
2020-03-17 02:01:18 -0400

    Add GET request for accounts from DB

--- accounts/account-router.js
+++ accounts/account-router.js
@@ -4,4 +4,14 @@ const db = require('../data/dbConfig.js');
 
 const router = express.Router();
 
+router.get('/', (req, res) => {
+    db.select('*').from('accounts')
+    .then(rows => {
+        res.status(200).json({ accounts: rows })
+    })
+    .catch(() => {
+        res.status(500).json({ message: "Sorry, Could not retrieve accounts" })
+    })
+})
+
 module.exports = router
